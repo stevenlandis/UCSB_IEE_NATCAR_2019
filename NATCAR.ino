262a24f4027e3f79ad65b4d9eccb91bba92bfbd2
Nina H <hmn.nini1970@gmail.com>
2019-02-08 16:35:48 -0800

    integrated files

--- NATCAR.ino
+++ NATCAR.ino
@@ -1,5 +1,6 @@
 #include <SPI.h>
 #include <avr/pgmspace.h>
+#include <math.h>
 
 // Registers
 #define REG_Product_ID                           0x00
@@ -67,6 +68,12 @@ unsigned long timeInterval = 0;
 extern const unsigned short firmware_length;
 extern const unsigned char firmware_data[];
 
+int PWM = 9;  //  pin for PWM
+int interruptPin = 10, wantedVelocity;
+double err, err_last, Kp, Ki, Kd, voltage, integral;
+float maxV = 5.0;
+float scale = 255/maxV;
+
 void setup() {
   Serial.begin(9600);
   
@@ -87,6 +94,11 @@ void setup() {
   adns_write_reg(0x02,0);
   adns_write_reg(REG_Configuration_I,resolution); //sets resolution to 1800 counts per inch
 
+  err = err_last = integral = 0;
+  wantedVelocity = 0;
+  Kp = Ki = Kd = 1;           // SHOULD BE TESTED
+  pinMode(PWM, OUTPUT);
+  Serial1.begin(38400);
 }
 
 void loop() {
@@ -120,11 +132,26 @@ void loop() {
 //  Serial.println(velocityx(distancex(convTwosComp(xydat[0])), timeInterval));
 //  Serial.print("Y-velocity: ");
 //  Serial.println(velocityy(distancey(convTwosComp(xydat[1])), timeInterval));
+  double xvelocity = velocityx(distancex(convTwosComp(xydat[0])), timeInterval);
+  double yvelocity = velocityy(distancey(convTwosComp(xydat[1])), timeInterval);
+  double velocity = sqrt(pow(xvelocity,2) + pow(yvelocity,2));
+  if(Serial1.available() > 0){
+    wantedVelocity = Serial.read() * scale;
+  }
+  analogWrite(PIDcontrol(velocity, wantedVelocity), PWM);
   movementflag = 0;
  }
  else if(!digitalRead(mot)){
   UpdatePointer();
  }
+}
+
+double PIDcontrol(double currV, int actualV){
+  err = currV - actualV;
+  integral += err;
+  double delta_err = err - err_last;
+  err_last = err; 
+  return actualV * (Kp * err + Ki * integral + Kd * delta_err);
 }
 
 void adns_com_begin(){ //communication start
