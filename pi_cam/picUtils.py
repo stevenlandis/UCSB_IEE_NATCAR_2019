e5f25c1c0b767fc7d263d2dc409f67b8441e9c94
stevenlandis <stevenlandis@comcast.net>
2019-02-11 13:51:46 -0800

    calcualte turn from image

--- pi_cam/picUtils.py
+++ pi_cam/picUtils.py
@@ -1,14 +1,40 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageOps
 import numpy as np
 from ScannerRect import ScannerRect
 import math
+import matplotlib.pyplot as plt
+
+def plotPixelHist(imgPath):
+    img = Image.open(imgPath)
+    img = ImageOps.grayscale(img)
+    data = np.array(img.getdata())
+    plt.hist(data)
+    plt.title('Intensity plot for "{}"'.format(imgPath))
+    plt.show()
 
 threshold = 107
 
 img = Image.open('img.jpg')
+img = ImageOps.grayscale(img)
 
 # threshold
-img = img.point(lambda p: p > threshold and 255).convert('L')
+img = img.point(lambda p: p > threshold and 255)
 
-s = ScannerRect(2, 4, 0, 0, 0)
-s.scan(lambda x,y: print(x,y), 1, 0, math.pi/2, 112, 112, 1)
+# get the pixel data
+data = np.array(img)
+
+xSum = 0
+xCount = 0
+for x in range(img.width):
+    for y in range(img.height):
+        if data[y][x]:
+            xSum += x
+            xCount += 1
+print(2*xSum/xCount/img.width-1)
+
+plt.imshow(data)
+plt.show()
+# img.show()
+
+# s = ScannerRect(2, 4, 0, 0, 0)
+# s.scan(lambda x,y: print(x,y), 1, 0, math.pi/2, 112, 112, 1)
